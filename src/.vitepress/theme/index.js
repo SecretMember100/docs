deef2b000e649700a5c336cd314686af3e19ee81
SecretMember100 <124039721+SecretMember100@users.noreply.github.com>
2023-01-31 18:01:30 +0530

    Create index.js

---
+++ src/.vitepress/theme/index.js
@@ -0,0 +1,14 @@
+import { h } from 'vue';
+import Theme from 'vitepress/theme';
+import '../style/main.css'
+import '../style/vars.css'
+import HomePage from '../components/HomePage.vue'
+
+export default {
+    ...Theme,
+    Layout() {
+        return h(Theme.Layout, null, {
+            'home-features-after': () => h(HomePage),
+        });
+    },
+};
